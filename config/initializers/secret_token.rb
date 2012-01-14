4c0b81ef344d51d8dc0439d5d575a21dfe7297f0
Miikka <miikka@saunis.net>
2012-01-14 17:52:33 +0200

    add starting files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arvi::Application.config.secret_token = 'e11da98ef692806dd2efdaf2a691edaa63356c5e8f52fb1798edac37428f785f99e2d41739b37684f6aeaecb76208a278bb50af3c4b06ae3258f8bd336213890'
